05350d104eb0f36dca70feae4934875046f695e7
adventur3 <wydn12345@qq.com>
2020-03-23 00:24:41 +0800

    DoublePartitionShortcut2

--- src/main/java/shortcuts/DoublePartitionShortcut2.java
+++ src/main/java/shortcuts/DoublePartitionShortcut2.java
@@ -1,15 +1,16 @@
 package shortcuts;
 
 import astar.AStar;
+import astar.AStarInfoNode;
 import astar.RestrainedAStar;
 import org.jgrapht.Graph;
-import roadNetwork.Path;
-import roadNetwork.PathSegment;
-import roadNetwork.RoadEdge;
-import roadNetwork.RoadNode;
+import roadNetwork.*;
 import simulator.SimClock;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /*
  * the start node's incoming core is also the target node's outgoing core then return true
@@ -37,14 +38,62 @@ public class DoublePartitionShortcut2 {
         return RestrainedAStar.singlePath(g, start, target, coreNode);
     }
 
-    public static Path crossPartitionSearch(Graph<RoadNode, RoadEdge> g, RoadNode start, RoadNode target){
+//    public static Path crossPartitionSearch(Graph<RoadNode, RoadEdge> g, RoadNode start, RoadNode target){
+//        RoadNode startCore = start.getBelongTo_incoming();
+//        RoadNode targetCore = target.getBelongTo();
+//        Path path1 = RestrainedAStar.singlePath(g, start, startCore, startCore);
+//        Path path2 =startCore.getCoreNode().getPath(targetCore.getCoreNode());
+//        path2 = getLongestPathToRegin(target,path2);
+//        Path path3 = RestrainedAStar.singlePath(g, path2.getTargetNode(), target, targetCore);
+//        return Path.pathCombine(Path.pathCombine(path1,path2),path3);
+//    }
+
+        public static Path crossPartitionSearch(Graph<RoadNode, RoadEdge> g, RoadNode start, RoadNode target){
         RoadNode startCore = start.getBelongTo_incoming();
         RoadNode targetCore = target.getBelongTo();
-        Path path1 = RestrainedAStar.singlePath(g, start, startCore, startCore);
+        Path path1 = null;
+        if(!start.isCore()){
+            path1 = step1(g, start, target);
+            startCore = path1.getTargetNode();
+        }
+        if(startCore == target){
+            return path1;
+        }
         Path path2 =startCore.getCoreNode().getPath(targetCore.getCoreNode());
         path2 = getLongestPathToRegin(target,path2);
         Path path3 = RestrainedAStar.singlePath(g, path2.getTargetNode(), target, targetCore);
         return Path.pathCombine(Path.pathCombine(path1,path2),path3);
+    }
+
+
+    public static Path step1(Graph<RoadNode, RoadEdge> g, RoadNode start, RoadNode target){
+        if(start.isCore()){
+            return null;
+        }else{
+            Map<String, AStarInfoNode> infoNodes=new HashMap<String,AStarInfoNode>();  //为了根据roadNode找到infoNode
+            LinkedList<AStarInfoNode> priorityQueue = new LinkedList<AStarInfoNode>();
+            long estimatedWeight = (long) (MillerCoordinate.distance(start, target));
+            AStarInfoNode startInfo = new AStarInfoNode(start, null, target, 0, estimatedWeight);
+            infoNodes.put(start.getOsmId(),startInfo);
+            priorityQueue.add(startInfo);
+            startInfo.setExplored();
+
+            Path path =null;
+            while(!priorityQueue.isEmpty()) {
+                AStarInfoNode inode = priorityQueue.pollFirst();
+                inode.setSetted();
+                if (inode.getRoadNode() == target) {
+                    path = AStar.outShortestPath(infoNodes, start, target);
+                    return path;
+                }
+                if (inode.getRoadNode().isCore()) {
+                    path = AStar.outShortestPath(infoNodes, start, inode.getRoadNode());
+                    return path;
+                }
+                AStar.updateForwardPriorityQueue(g, infoNodes, priorityQueue, inode, target);
+            }
+        }
+        return null;
     }
 
     public static Path getLongestPathToRegin(RoadNode regionNode, Path path){
